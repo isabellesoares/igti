e140b8ea73c53b5ab88584e6e72ed4c80b68c16f
Isabelle Soares <isabelle.soares@sistemafiep.org.br>
2021-03-09 22:13:54 -0300

    git pull origin maste

---
+++ tiposVariaveis.js
@@ -0,0 +1,25 @@
+//Aprendendo sobre os tipos de variavel
+var inteiro = 10;
+var decimal = 10.39;
+var stringAspasDuplas = "texto de teste aspa duplas"; 
+var stringAspasSimples = 'texto de teste aspa simples';
+var booleanoVerdadeiro = true;
+var booleanoFalso = false;
+var variavelNaoDefinida;
+var variavelNula = null;
+
+//imprimindo as variaveis no console
+console.log(inteiro);
+console.log(decimal);
+console.log(stringAspasDuplas);
+console.log(stringAspasSimples);
+console.log(booleanoVerdadeiro);
+console.log(booleanoFalso);
+console.log(variavelNaoDefinida);
+console.log(variavelNula);
+
+var mudancaTipo = 1;
+console.log(mudancaTipo);
+mudancaTipo = "Troquei de numero para texto";
+console.log(mudancaTipo);
+
