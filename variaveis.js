949dd0c5708f255433b41e5b55234682189870c8
Isabelle Soares <isabelle.soares@sistemafiep.org.br>
2021-03-09 21:54:42 -0300

    pull origin master

--- variaveis.js
+++ variaveis.js
@@ -1,7 +1,15 @@
+//declarando variaveis
 var abacate = 2;
 var banana = 3;
+
+//declarando variavel da soma
 var totalFrutasSacola = abacate + banana;
+
+//trocando valor da variavel abacate
 abacate = 5;
+
+//somando novamente
 totalFrutasSacola = abacate + banana;
 
+//apresentando o valor no console
 console.log(totalFrutasSacola);
