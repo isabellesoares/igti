d3f26b1faeff3973ff180c81a566b7ba6cc86c43
Isabelle Soares <isabelle.soares@sistemafiep.org.br>
2021-03-11 22:06:56 -0300

    Atualização e criação de arquivos

--- calculadora.js
+++ calculadora.js
@@ -1,6 +1,7 @@
-var operacao = "soma";
-var num1 = 4;
-var num2 = 3;
+//console.log(process.argv);
+var operacao = process.argv[2];
+var num1 = parseInt(process.argv[3]);
+var num2 = parseInt(process.argv[4]);
 var resultado = 0;
 
 if(operacao === "soma"){
@@ -11,5 +12,9 @@ if(operacao === "soma"){
     resultado = num1 * num2;
 } else if(operacao === "divisao"){
     resultado = num1 / num2;
+}else if(operacao === "resto"){
+    resultado = num1 % num2;
+}else if (operacao === "porcentagem"){
+    resultado = num1 * num2 / 100;
 }
 console.log(resultado);
