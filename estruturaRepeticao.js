940baa9868af31f661dffe1f8cc8c9dc2b08f581
Isabelle Soares <isabelle.soares@sistemafiep.org.br>
2021-03-14 19:40:56 -0300

    Novas aulas e Project Euller

---
+++ estruturaRepeticao.js
@@ -0,0 +1,59 @@
+for (var i = 0; i < 10; i++) {
+    //console.log(i);
+    
+}
+///do começo para o final do array
+var carros = ["Gol","Palio", "Uno", "Celta"];
+for(var i = 0; i < carros.length; i++){
+   // console.log(carros[i]);
+}
+///do fim para o inicio do array
+for(var i = carros.length - 1; i >= 0; i--){
+   // console.log(carros[i]);
+}
+
+var i = 0;
+while (i < carros.length) {
+    //console.log(carros[i]);
+    i++;
+}
+
+/*while (true) {
+    //console.log("Olá");
+}*/
+
+var i = 0;
+while (i < carros.length) {
+    //console.log(carros[i]);
+    if(carros[i] === "Palio"){
+        //console.log("Encontrei o Palio");
+        break;//interrompe laço ao encontrar o valor
+    }
+    i++;
+}
+
+
+var numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+var i = 0;
+while (i < numeros.length) {
+    var num = numeros[i];
+    i++;
+    
+    if(num % 2 === 0){
+        //console.log(num + " é par");
+        continue;
+    }
+        //console.log(num + " é ímpar");
+}
+
+var j = 0;
+do {
+    j++;
+   // console.log(j);
+} while (j < 10);
+
+//var carros = ["Gol","Palio", "Uno", "Celta"];
+
+for(var carro of carros){
+    console.log(carro);
+}
